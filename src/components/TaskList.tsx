8a85e0499314d5b520516b3d388cef87ecd9a284
Jhonata Costa <jhonata_costa@econeteditora.com.br>
2022-08-22 17:16:14 -0300

    adicionando na lista

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -3,13 +3,15 @@ import styles from './TaskList.module.css';
 
 import { TbTrash } from 'react-icons/tb' 
 import { useState } from 'react';
+import { Task } from '../App';
 
 interface TaskListProps {
   taskCounter: number;
   concludedTaskCounter: number;
+  taskList: Task[];
 }
 
-export function TaskList ({ taskCounter, concludedTaskCounter }: TaskListProps) {
+export function TaskList ({ taskCounter, concludedTaskCounter, taskList }: TaskListProps) {
   const  [isCheked, setIsCheked] = useState(false)
 
   function handleCheckBox () {
@@ -40,16 +42,22 @@ export function TaskList ({ taskCounter, concludedTaskCounter }: TaskListProps) 
           <strong>Você ainda não tem tarefas cadastradas </strong>
           <p>Crie tarefas e organize seus itens a fazer</p>
         </div> :
-          <div className={styles.hasTasks}>
+        <>
+          {taskList.map(task => {
+            return (
+              <div key={task.id} className={styles.hasTasks}>
           <div className={styles.checkContainer}>
             <input type="checkbox"  checked={isCheked} onChange={handleCheckBox} />
             <span className={styles.checkmark} ></span>
           </div>
-          <label className={isCheked ? styles.checkedLabel : undefined }  >
-            Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.
+          <label className={task.taskDone ? styles.checkedLabel : undefined }  >
+            {task.taskDescription}
           </label>
           <TbTrash />
           </div>
+            )
+          })}
+        </>
         } 
 
        
